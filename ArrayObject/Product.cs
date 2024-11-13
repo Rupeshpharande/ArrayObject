a6661aceb3534b46a711a3832b42dfdc6ef5a07c
Rupeshpharande <pharanderupesh@gmail.com>
2024-11-14 01:10:48 +0530

    Add project files.

---
+++ ArrayObject/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayObject
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Pname { get; set; }
+
+        public double Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID ={Id} ,ProductName= {Pname} ,Price ={Price}";
+
+        }
+    }
+}
---
+++ ArrayObject/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Product[] pro = new Product[]
+            //{
+            //    new Product{Id=1,Pname="Fan",Price=2300},
+            //    new Product{Id=2,Pname="TV",Price=15000 },
+            //    new Product{Id=3,Pname="chair",Price=400 },
+            //    new Product{Id=4,Pname="Fridge",Price=16000 },
+            //    new Product{Id=5,Pname="Table",Price=4000 }
+
+            //};
+
+            ////Display all products - using for & foreach
+
+            //Console.WriteLine("using forLoop");
+           
+            //for(int i=0;i<pro.Length;i++)
+            //{
+            //    Console.WriteLine(pro[i]);
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("using foreach");
+
+            //foreach(Product p in pro) 
+            //{ 
+            //    Console.WriteLine(p);
+            //}
+
+            ////Display the products whose price is > 500 -foreach
+            //Console.WriteLine();
+            //Console.WriteLine("Display the products whose price is > 500");
+            //foreach(Product p in pro)
+            //{
+            //    if(p.Price>500)
+            //    {
+            //        Console.WriteLine(p);
+            //    }
+            //}
+
+            //Employee [] employees = new Employee[]
+            //{
+            //    new Employee{Id=1, Name="Rupesh",Salary=50000},
+            //    new Employee{Id=2, Name="Rohan",Salary=40000},
+            //    new Employee{Id=3, Name="Atharv",Salary=55000},
+            //    new Employee{Id=4, Name="Arnav",Salary=35000},
+            //    new Employee{Id=5, Name="Akshay",Salary=60000},
+            //};
+
+            ////for (int i = 0; i < employees.Length; i++)
+            ////{
+            ////    Console.WriteLine(employees[i]);
+            ////}
+            //Console.WriteLine();
+            //foreach (Employee emp in employees)
+            //{
+            //    Console.WriteLine(emp);
+            //}
+
+
+
+        }
+    }
+}
